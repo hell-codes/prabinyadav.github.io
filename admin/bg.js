643d410c5b2175c9e11b2dbc983bd8568817fe17
Prabin Yadav <prabin.yadav935@gmail.com>
2026-04-03 21:23:21 +0530

    Change canvas context from 2d to 3d

--- admin/bg.js
+++ admin/bg.js
@@ -1,5 +1,5 @@
 const canvas = document.getElementById("adminBg");
-const ctx = canvas.getContext("2d");
+const ctx = canvas.getContext("3d");
 
 let particles = [];
 let mouse = { x: null, y: null };
